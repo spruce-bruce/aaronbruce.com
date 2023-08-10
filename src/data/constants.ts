4b8e24329574d8bdf130c6b937117e61c91c2269
Aaron Bruce <aaron@synapsestudios.com>
2023-08-10 15:09:15 -0700

    better readme

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	github: "https://github.com/spruce-bruce",
+	twitter: "https://twitter.com/TerribleAaron",
+	linkedin: "https://www.linkedin.com/in/aarondbruce/",
+	mastodon: "https://hachyderm.io/@TerribleAaron",
 };
